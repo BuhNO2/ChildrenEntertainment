4a1f58e09ab86b31b666bdbef74901a692ae67e7
Racy <valeralev20178@gmail.com>
2022-06-16 12:00:45 +0500

    a

---
+++ DataBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kids
+{
+    class DataBase
+    {
+        public bool ExecuteCommand(MySqlCommand command)
+        {
+            bool b1 = true;
+            try
+            {
+                variables.Connection.Open();
+                command.ExecuteNonQuery();
+
+            }
+            catch (Exception x) { MessageBox.Show(x.ToString()); b1 = false; }
+            if (variables.Connection.State == ConnectionState.Open)
+                variables.Connection.Close();
+            return b1;
+
+        }
+
+    }
+}
---
+++ variables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Users;
+
+namespace Kids
+{
+    class variables
+    {
+        public static CurrentUser user = new CurrentUser();
+        public static MySqlConnection Connection;
+        public static string filepath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"ChildEntertain\Server");
+
+    }
+}
